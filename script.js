e7361ca381aa490128260d79253a9c25a7e672b2
Mike de Vries <mbv.spike@gmail.com>
2025-03-19 16:06:00 +0100

    added nice feature calculation
    - added caculation of form advanced form with local storage

--- script.js
+++ script.js
@@ -1,3 +1,143 @@
+const balanceInheritanceFields = document.querySelectorAll(
+    ".balance-inheritance input"
+);
+const totalValues = document.querySelectorAll(".balance-inheritance p");
+
+// Balance Inheritance Input Fields
+
+// VALUE input fields
+const generalInboedelField = document.querySelector("input[name='g-inboedel']");
+const privateInboedelField = document.querySelector("input[name='p-inboedel']");
+const generalVervoersmiddelField = document.querySelector(
+    "input[name='g-vervoersmiddel']"
+);
+const privateVervoersmiddelField = document.querySelector(
+    "input[name='p-vervoersmiddel']"
+);
+
+const generalKunstField = document.querySelector("input[name='g-kunst']");
+const privateKunstField = document.querySelector("input[name='p-kunst']");
+const generalOverigField = document.querySelector("input[name='g-overig']");
+const privateOverigField = document.querySelector("input[name='p-overig']");
+
+// DEBT Input Fields
+
+const generalMortgageField = document.querySelector("input[name='g-mortgage']");
+const privateMortgageField = document.querySelector("input[name='p-mortgage']");
+const generalGiftField = document.querySelector("input[name='g-gift']");
+const privateGiftField = document.querySelector("input[name='p-gift']");
+
+// FUNEREAL EXPENSES Input Fields
+
+const ceremonyField = document.querySelector("input[name='ceremony']");
+const cateringField = document.querySelector("input[name='catering']");
+
+// AUTHORIZED PERSONS Input Fields
+
+const authorizedPersonsField = document.querySelector(
+    "input[name='authorized-persons']"
+);
+
+// TOTAL VALUES
+
+const totalBalanceInheritance = document.querySelector(
+    "p[data-total-balance-inheritance]"
+);
+const totalPersonalBalanceInheritance = document.querySelector(
+    "p[data-total-personal-balance-inheritance]"
+);
+
+
+
+totalValues.forEach((value) => {
+    value.removeAttribute("hidden");
+});
+
+
+// Balance Inheritance Calculation
+
+// FUNCTIES OM TUSSENTOTALEN TE BEREKENEN
+
+function totalValueCalculation() {
+    const value =
+        (Number(generalInboedelField.value) +
+            Number(generalVervoersmiddelField.value) +
+            Number(generalKunstField.value) +
+            Number(generalOverigField.value)) /
+        4 +
+        Number(privateInboedelField.value) +
+        Number(privateVervoersmiddelField.value) +
+        Number(privateKunstField.value) +
+        Number(privateOverigField.value);
+
+    return value;
+}
+
+function totalDebtCalculation() {
+    const debt =
+        (Number(generalMortgageField.value) + Number(generalGiftField.value)) / 2 +
+        Number(privateMortgageField.value) +
+        Number(privateGiftField.value);
+
+    return debt;
+}
+
+function totalFuneralExpensesCalculation() {
+    const funeralExpenses =
+        Number(ceremonyField.value) + Number(cateringField.value);
+
+    return funeralExpenses;
+}
+
+function totalBalanceInheritanceCalculation() {
+    const balanceInheritance =
+        totalValueCalculation() -
+        totalDebtCalculation() -
+        totalFuneralExpensesCalculation();
+
+    return balanceInheritance;
+}
+
+function totalPersonalBalanceInheritanceCalculation() {
+    let authorized = 1;
+    if (authorizedPersonsField.value < 2) {
+        authorized = 1;
+    } else {
+        authorized = authorizedPersonsField.value;
+    }
+
+    const total = totalBalanceInheritanceCalculation() / authorized;
+
+    return total;
+}
+
+// TOTALE BEREKENING
+
+function calculateTotal() {
+
+    totalBalanceInheritance.textContent = `Totaal: €${totalBalanceInheritanceCalculation().toFixed(2)}`;
+
+    totalPersonalBalanceInheritance.textContent = `Per persoon: €${totalPersonalBalanceInheritanceCalculation().toFixed(2)}`;
+}
+
+balanceInheritanceFields.forEach((field) => {
+    document.addEventListener("DOMContentLoaded", () => {
+        calculateTotal();
+    });
+
+    field.addEventListener("input", () => {
+        calculateTotal();
+    });
+});
+
+
+
+
+
+
+
+
+
 // let overigSelected = document.querySelector('.overigInput')
 // let inputsSelected = document.querySelector('.inputsSelected')
 // let inputFieldOverig = document.querySelector('.overigTextArea')
@@ -12,6 +152,7 @@
 //     inputFieldOverig.toggleAttribute = hidden;
 //     // inputFieldOverig.classList.add('fieldAnimation')
 // })
+
 
 
 let bedrijfJa = document.querySelector("#bedrijfJa");
@@ -75,3 +216,12 @@ inputFields.forEach((inputField) => {
 // inputNee.addEventListener("click", (event) => {
 //     bedrijfsVragen.hidden = true;
 // })
+
+
+
+
+
+
+
+
+
